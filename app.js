6bace037fc462eed0bd1d5873a332fb92989b3f4
billsanthony <111157366+billsanthony@users.noreply.github.com>
2023-12-16 12:57:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,19 @@
-// Sample data structure to store athlete performances
-let performances = [];
+// Fetch athletes from the backend and populate the dropdown
+async function populateAthletesDropdown() {
+    const response = await fetch('http://localhost:3000/api/athletes');
+    const athletes = await response.json();
 
-function addPerformance() {
-    const athleteName = document.getElementById('athleteName').value;
-    const performanceMetric = document.getElementById('performanceMetric').value;
+    const playerSelect = document.getElementById('playerSelect');
+    playerSelect.innerHTML = '<option value="">Select Player</option>';
 
-    // Basic validation
-    if (!athleteName || !performanceMetric) {
-        alert('Please enter both athlete name and performance metric.');
-        return;
-    }
-
-    // Create a performance object
-    const performance = {
-        athlete: athleteName,
-        metric: performanceMetric,
-        date: new Date().toLocaleDateString()
-    };
-
-    // Add performance to the list
-    performances.push(performance);
-
-    // Display performances
-    displayPerformances();
-
-    // Clear input fields
-    document.getElementById('athleteName').value = '';
-    document.getElementById('performanceMetric').value = '';
-
-    // Commit to GitHub
-    commitToGitHub();
-}
-
-function displayPerformances() {
-    const performanceList = document.getElementById('performanceList');
-    performanceList.innerHTML = '';
-
-    performances.forEach(performance => {
-        const performanceItem = document.createElement('div');
-        performanceItem.innerHTML = `<strong>${performance.athlete}</strong> - ${performance.metric} (${performance.date})`;
-        performanceList.appendChild(performanceItem);
+    athletes.forEach(athlete => {
+        const option = document.createElement('option');
+        option.value = athlete.name;
+        option.textContent = athlete.name;
+        playerSelect.appendChild(option);
     });
 }
 
-// Simulate a basic GitHub commit
-function commitToGitHub() {
-    // This is a placeholder for the GitHub commit functionality.
-    // In a real-world scenario, you'd use Git commands or a library like Octokit.
-    console.log('Committed to GitHub');
-}
+// Fetch and display performances for the selected player
+async function showPlayerPerformance() {
+   
