72f3454050b30473cc0d297019567e7d004677b9
billsanthony <111157366+billsanthony@users.noreply.github.com>
2023-12-21 10:52:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const fetch = require('node-fetch');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect('your_database_url', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const athleteSchema = new mongoose.Schema({
+    name: String,
+    performance: {
+        metric: String,
+        value: Number
+    }
+});
+
+const Athlete = mongoose.model('Athlete', athleteSchema);
+
 // Function to generate random athlete data
 function generateRandomAthlete() {
-    const names = [
-        'Cristiano Ronaldo', 'Lionel Messi', 'Neymar Jr.', 'Robert Lewandowski', 'Mohamed Salah',
-        'Kylian Mbappe', 'Kevin De Bruyne', 'Harry Kane', 'Erling Haaland', 'Sadio Mane'
-        // Add more footballers or athletes as needed
-    ];
-
+    const names = ['Cristiano Ronaldo', 'Lionel Messi', 'Neymar Jr.', 'Robert Lewandowski', 'Mohamed Salah'];
     const metrics = ['Goals', 'Assists', 'Points', 'Aces', 'Sprints'];
 
     const randomName = names[Math.floor(Math.random() * names.length)];
@@ -39,27 +56,90 @@ function getRandomValue(metric) {
     }
 }
 
+// Fetch real footballer data from the Football Data API
+async function fetchRealFootballerData() {
+    try {
+        const response = await fetch('https://api.football-data.org/v2/competitions/PL/scorers?limit=10', {
+            method: 'GET',
+            headers: {
+                'X-Auth-Token': 'your_api_key', // Replace with your actual API key
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+
+        // Adapt the response to fit the athlete data structure
+        const adaptedData = data.scorers.map((scorer) => ({
+            name: scorer.player.name,
+            performance: {
+                metric: 'Goals',
+                value: scorer.numberOfGoals,
+            },
+        }));
+
+        return adaptedData;
+    } catch (error) {
+        console.error('Error fetching real footballer data:', error);
+        throw error;
+    }
+}
+
 // Function to show random performances in tabular form
 async function showRandomPerformances() {
-    const randomAthlete = generateRandomAthlete();
+    try {
+        const randomAthlete = generateRandomAthlete();
 
-    const performanceList = document.getElementById('performanceList');
-    performanceList.innerHTML = '';
+        const performanceList = document.getElementById('performanceList');
+        performanceList.innerHTML = '';
 
-    const performanceTableBody = document.getElementById('performanceTableBody');
-    performanceTableBody.innerHTML = '';
+        const performanceTableBody = document.getElementById('performanceTableBody');
+        performanceTableBody.innerHTML = '';
 
-    // Display in a div
-    const athleteDiv = document.createElement('div');
-    athleteDiv.innerHTML = `<strong>${randomAthlete.name}</strong> - ${randomAthlete.performance.metric}: ${randomAthlete.performance.value}`;
-    performanceList.appendChild(athleteDiv);
+        // Display in a div
+        const athleteDiv = document.createElement('div');
+        athleteDiv.innerHTML = `<strong>${randomAthlete.name}</strong> - ${randomAthlete.performance.metric}: ${randomAthlete.performance.value}`;
+        performanceList.appendChild(athleteDiv);
 
-    // Display in a table
-    const row = document.createElement('tr');
-    row.innerHTML = `
-        <td>${randomAthlete.name}</td>
-        <td>${randomAthlete.performance.metric}</td>
-        <td>${randomAthlete.performance.value}</td>
-    `;
-    performanceTableBody.appendChild(row);
+        // Display in a table
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${randomAthlete.name}</td>
+            <td>${randomAthlete.performance.metric}</td>
+            <td>${randomAthlete.performance.value}</td>
+        `;
+        performanceTableBody.appendChild(row);
+    } catch (error) {
+        console.error('Error showing random performances:', error);
+    }
 }
+
+// Function to show footballer statistics in tabular form
+async function showFootballerStatistics() {
+    try {
+        const footballerData = await fetchRealFootballerData();
+
+        const performanceList = document.getElementById('performanceList');
+        performanceList.innerHTML = '';
+
+        const performanceTableBody = document.getElementById('performanceTableBody');
+        performanceTableBody.innerHTML = '';
+
+        // Display in a table
+        footballerData.forEach((footballer) => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${footballer.name}</td>
+                <td>${footballer.performance.metric}</td>
+                <td>${footballer.performance.value}</td>
+            `;
+            performanceTableBody.appendChild(row);
+        });
+    } catch (error) {
+        console.error('Error showing footballer statistics:', error);
+    }
+}
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
