36eb5e8ed2b4d1fc17bb5b18802caf3c16af153f
billsanthony <111157366+billsanthony@users.noreply.github.com>
2023-12-21 11:43:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,30 @@
 // Add this code to your app.js
 
-// Function to fetch unique values for dropdowns from the database
-async function fetchUniqueValues() {
+// Function to show random performances in tabular form
+async function showRandomPerformances() {
     try {
-        const uniqueNames = await Athlete.distinct('name');
-        const uniqueSportCategories = await Athlete.distinct('sportCategory');
-        const uniqueGenders = await Athlete.distinct('gender');
+        const randomAthlete = generateRandomAthlete();
 
-        return { uniqueNames, uniqueSportCategories, uniqueGenders };
+        const performanceList = document.getElementById('performanceList');
+        performanceList.innerHTML = '';
+
+        const performanceTableBody = document.getElementById('performanceTableBody');
+        performanceTableBody.innerHTML = '';
+
+        // Display in a div
+        const athleteDiv = document.createElement('div');
+        athleteDiv.innerHTML = `<strong>${randomAthlete.name}</strong> - ${randomAthlete.performance.metric}: ${randomAthlete.performance.value}`;
+        performanceList.appendChild(athleteDiv);
+
+        // Display in a table
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${randomAthlete.name}</td>
+            <td>${randomAthlete.performance.metric}</td>
+            <td>${randomAthlete.performance.value}</td>
+        `;
+        performanceTableBody.appendChild(row);
     } catch (error) {
-        console.error('Error fetching unique values for dropdowns:', error);
-        throw error;
+        console.error('Error showing random performances:', error);
     }
 }
-
-// Function to populate dropdown options
-async function populateDropdowns() {
-    try {
-        const { uniqueNames, uniqueSportCategories, uniqueGenders } = await fetchUniqueValues();
-
-        // Populate dropdowns
-        populateDropdown('athleteNameDropdown', uniqueNames);
-        populateDropdown('sportCategoryDropdown', uniqueSportCategories);
-        populateDropdown('genderDropdown', uniqueGenders);
-    } catch (error) {
-        console.error('Error populating dropdowns:', error);
-    }
-}
-
-// Function to populate a dropdown with options
-function populateDropdown(dropdownId, options) {
-    const dropdown = document.getElementById(dropdownId);
-
-    // Clear existing options
-    dropdown.innerHTML = '';
-
-    // Add default option
-    const defaultOption = document.createElement('option');
-    defaultOption.text = 'Select';
-    defaultOption.value = '';
-    dropdown.add(defaultOption);
-
-    // Add options from the array
-    options.forEach((option) => {
-        const newOption = document.createElement('option');
-        newOption.text = option;
-        newOption.value = option;
-        dropdown.add(newOption);
-    });
-}
-
-// Call the function to populate dropdowns
-populateDropdowns();
