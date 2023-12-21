b9e96b33b59c1229f96fa1dd50958c0d862e96d8
billsanthony <111157366+billsanthony@users.noreply.github.com>
2023-12-21 12:10:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
 // Existing code...
 
-// Function to show random performances in tabular form
-async function showRandomPerformances() {
+// Function to show random performances for the new tab
+async function showNewTabPerformances() {
     try {
-        // Generate a random index
-        const randomIndex = Math.floor(Math.random() * 2); // Either 0 or 1
-
-        let randomAthlete;
-        if (randomIndex === 0) {
-            // Use existing random athlete data generation
-            randomAthlete = generateRandomAthlete();
-        } else {
-            // Fetch real footballer data from the Football Data API
-            randomAthlete = await fetchRealFootballerData();
-        }
+        const randomAthlete = generateNewTabRandomAthlete();
 
         const performanceList = document.getElementById('performanceList');
         performanceList.innerHTML = '';
@@ -35,8 +25,26 @@ async function showRandomPerformances() {
         `;
         performanceTableBody.appendChild(row);
     } catch (error) {
-        console.error('Error showing random performances:', error);
+        console.error('Error showing new tab performances:', error);
     }
+}
+
+// Function to generate random athlete data for the new tab
+function generateNewTabRandomAthlete() {
+    const names = ['Usain Bolt', 'Serena Williams', 'Michael Phelps', 'Simone Biles', 'Floyd Mayweather'];
+    const metrics = ['Gold Medals', 'Grand Slam Titles', 'Olympic Golds', 'Championships', 'Undefeated Matches'];
+
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    const randomMetric = metrics[Math.floor(Math.random() * metrics.length)];
+    const randomValue = getRandomValue(randomMetric);
+
+    return {
+        name: randomName,
+        performance: {
+            metric: randomMetric,
+            value: randomValue
+        }
+    };
 }
 
 // Existing code...
