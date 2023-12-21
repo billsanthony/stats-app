06a53fdbce11ce4fee00f4b12de37801d97e5517
billsanthony <111157366+billsanthony@users.noreply.github.com>
2023-12-21 21:50:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,81 +1,35 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const fetch = require('node-fetch');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// MongoDB setup (replace 'your_database_url' with your actual MongoDB connection string)
-mongoose.connect('your_database_url', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
+// Express Middleware
+app.use(express.static('public'));
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// Dummy Athletes Data (Replace with MongoDB)
+const topAthletes = generateTopAthletes();
 
-const athleteSchema = new mongoose.Schema({
-    name: String,
-    performance: {
-        metric: String,
-        value: Number
-    }
+// API Endpoint to Fetch Top Athletes
+app.get('/api/top-athletes', (req, res) => {
+    res.json(topAthletes);
 });
 
-const Athlete = mongoose.model('Athlete', athleteSchema);
-
-// Fetch random athlete data from Sports Open Data API
-async function fetchRandomAthleteData() {
-    try {
-        const response = await fetch('https://sportsop-soccer-sports-open-data-v1.p.rapidapi.com/v1/leagues/premier-league/seasons/21-22/teams',
-            {
-                method: 'GET',
-                headers: {
-                    'X-RapidAPI-Host': 'sportsop-soccer-sports-open-data-v1.p.rapidapi.com',
-                    'X-RapidAPI-Key': 'your_rapidapi_key'
-                }
-            });
-
-        const data = await response.json();
-        const randomTeam = data[Math.floor(Math.random() * data.length)];
-        const randomAthlete = {
-            name: randomTeam.name,
-            performance: {
-                metric: 'Goals',
-                value: Math.floor(Math.random() * 10) + 1
-            }
-        };
-
-        return randomAthlete;
-    } catch (error) {
-        console.error('Error fetching random athlete data:', error);
-        throw error;
-    }
+// Function to generate top athlete data
+function generateTopAthletes() {
+    return [
+        { name: 'Cristiano Ronaldo', sport: 'Football', gender: 'Male', age: 36 },
+        { name: 'Lionel Messi', sport: 'Football', gender: 'Male', age: 34 },
+        { name: 'Serena Williams', sport: 'Tennis', gender: 'Female', age: 40 },
+        { name: 'Usain Bolt', sport: 'Track and Field', gender: 'Male', age: 35 },
+        { name: 'Simone Biles', sport: 'Gymnastics', gender: 'Female', age: 24 },
+        { name: 'LeBron James', sport: 'Basketball', gender: 'Male', age: 37 },
+        { name: 'Roger Federer', sport: 'Tennis', gender: 'Male', age: 40 },
+        { name: 'Tom Brady', sport: 'American Football', gender: 'Male', age: 44 },
+        { name: 'Naomi Osaka', sport: 'Tennis', gender: 'Female', age: 24 },
+        { name: 'Novak Djokovic', sport: 'Tennis', gender: 'Male', age: 34 },
+    ];
 }
 
-// API endpoint to get random athlete data
-app.get('/api/athletes/random', async (req, res) => {
-    try {
-        const randomAthlete = await fetchRandomAthleteData();
-        res.json([randomAthlete]);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-// API endpoint to search for an athlete by name
-app.get('/api/athletes/search', async (req, res) => {
-    const athleteName = req.query.name;
-
-    if (!athleteName) {
-        return res.status(400).json({ error: 'Please provide an athlete\'s name for search.' });
-    }
-
-    try {
-        const athletes = await Athlete.find({ name: new RegExp(athleteName, 'i') });
-        res.json(athletes);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
+// Start the Server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
